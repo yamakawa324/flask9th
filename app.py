bd04c35ee673c937010406e0d8961d7f46ff620c
yamakawa sato <saiaka0324@gmail.com>
2021-05-05 20:55:12 +0900

    html

--- app.py
+++ app.py
@@ -9,7 +9,7 @@
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
-from flask import Flask #ファイル名flaskでFlaskやるよ
+from flask import Flask, render_template #ファイル名flaskでFlaskやるよ 追加もこっちで！ *は全部！
 app = Flask(__name__) #アプリ名
 
 @app.route('/') #/にアクセスした時に、下の関数を動かす
@@ -32,6 +32,9 @@ def greet(name):
     text = name + "さんこんにちは"
     return text
 
+@app.route("/tpltest")
+def tpltest():
+    return render_template("index.html")
 
 
 
