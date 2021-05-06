1922eef922997dc426c582d15e1d010e50f1f263
yamakawa sato <saiaka0324@gmail.com>
2021-05-06 21:51:38 +0900

    404

--- app.py
+++ app.py
@@ -10,6 +10,7 @@
 #     app.run(debug=True)
 
 from flask import Flask, render_template #ファイル名flaskでFlaskやるよ 追加もこっちで！ *は全部！
+import sqlite3
 app = Flask(__name__) #アプリ名
 
 @app.route('/') #/にアクセスした時に、下の関数を動かす
@@ -27,10 +28,10 @@ def top():
     text = "ここはトップページです"
     return text
 
-@app.route("/<name>")
-def greet(name):
-    text = name + "さんこんにちは"
-    return text
+# @app.route("/<name>")
+# def greet(name):
+#     text = name + "さんこんにちは"
+#     return text
 
 @app.route("/tpltest/<name>")
 def tpltest(name):
@@ -43,6 +44,29 @@ def profile():
     address = "うるま市宮里842ー３"
     return render_template("profile.html", tpl_name=name, tpl_ago=ago, tpl_address=address)
 
+@app.route("/weathre")
+def weathre():
+    weathre1 = "晴れ"
+    return render_template("weathre.html", tpl_weather1=weathre1 )
+
+@app.route("/dbtest")
+def dbtest():
+    #データベース接続
+    conn = sqlite3.connect("dbtest.db")
+    #dbを操作できるように
+    c = conn.cursor()
+    #db実行
+    c.execute("select name,age,address from users where id = 2")
+    #dbで取得したデータを変数に格納
+    user_info = c.fetchone()
+    print(user_info)
+    #dbとの接続を終了
+    c.close()
+    return render_template("dbtest.html", tpl_name=user_info[0],tpl_age=user_info[1],tpl_address=user_info[2])
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html")
 
 
 ## おまじない
