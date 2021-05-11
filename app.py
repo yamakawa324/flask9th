73ffa501a17fe5b9f6ce42f6e93dc7d8cf02792b
yamakawa sato <saiaka0324@gmail.com>
2021-05-11 22:01:30 +0900

    ログイン機能をつけたよ

--- app.py
+++ app.py
@@ -9,9 +9,12 @@
 # if __name__ == "__main__":
 #     app.run(debug=True)
 
-from flask import Flask, render_template, request, redirect #ファイル名flaskでFlaskやるよ 追加もこっちで！ *は全部！
+from flask import Flask, render_template, request, redirect, session #ファイル名flaskでFlaskやるよ 追加もこっちで！ *は全部！
 import sqlite3
 app = Flask(__name__) #アプリ名
+
+#暗号化の秘密鍵を作成 ””の間はランダムで
+app.secret_key = "sunabaco"
 
 @app.route('/') #/にアクセスした時に、下の関数を動かす
 def hello():
@@ -90,24 +93,29 @@ def add():
 # dbたすく表示
 @app.route("/list")
 def list():
-     #データベース接続
-    conn = sqlite3.connect("dbtest.db")
-    #dbを操作できるように
-    c = conn.cursor()
-    #db実行
-    c.execute("select id,name from task")
-    #dbで取得したデータを変数に格納
-    task_info = c.fetchall()
-    print(task_info)
-    #dbとの接続を終了
-    c.close()
-    #task_listを辞書型として宣言
-    task_list = []
-    #タプルから辞書がたに整形
-    for item in task_info:
-        task_list.append({"id": item[0], "name": item[1]})
+    #ログインしていなければログインページに飛ばす
+    if "user_id" in session: #user_idがクッキーに保存されていたら
+         #データベース接続
+        conn = sqlite3.connect("dbtest.db")
+        #dbを操作できるように
+        c = conn.cursor()
+        #db実行
+        c.execute("select id,name from task")
+        #dbで取得したデータを変数に格納
+        task_info = c.fetchall()
+        print(task_info)
+        #dbとの接続を終了
+        c.close()
+        #task_listを辞書型として宣言
+        task_list = []
+        #タプルから辞書がたに整形
+        for item in task_info:
+            task_list.append({"id": item[0], "name": item[1]})
 
-    return render_template("list.html", tpl_task_list=task_list)
+        return render_template("list.html", tpl_task_list=task_list)
+        #ログインしていなかったら、ログインページにとばす
+    else:
+            return redirect("/loginpage")
 
 @app.route("/editpage/<task_id>")
 def editpage(task_id):
@@ -158,6 +166,59 @@ def delete(del_id):
 @app.route("/registpage")
 def registpage():
     return render_template("regist.html")
+
+#登録ボタンが押されたときの処理
+@app.route("/regist", methods=["POST"])
+def regist():
+    #入力フォームから名前とパスワードを受け取る
+    #DBに接続して、操作できるようにする
+    py_name = request.form.get("name")
+    py_pass = request.form.get("password")
+    #データベース接続
+    conn = sqlite3.connect("dbtest.db")
+    #dbを操作できるように
+    c = conn.cursor()
+    #1で受け取った名前とパスワードをaccountテーブルに挿入
+    c.execute("insert into account values(null,?,?)", (py_name, py_pass))
+    #DBを保存、接続終える
+    conn.commit()
+    c.close()
+    return "アカウント登録"
+
+@app.route("/loginpage")
+def loginpage():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    #login.htmlの入力ホームから名前とパスワードを受け取り
+    py_name = request.form.get("name")
+    py_pass = request.form.get("password")
+    #dB接続、操作
+    conn = sqlite3.connect("dbtest.db")
+    c = conn.cursor()
+    #1で受け取った名前とパスワードがどちらも一致するIDを
+    #accountテーブルから取得
+    c.execute("select id from account where name = ? and password = ? ", (py_name, py_pass))
+    #user＿idという変数に＃３で取得したIDを代入
+    user_id = c.fetchone()
+    print(user_id)
+    #DBとの接続終える
+    conn.commit()
+    c.close()
+    if user_id is None: #もし値が空だったら
+        return redirect("/loginpage")
+    else:
+        #ログイン情報をクッキーに保存（DBから取得したidをクッキーにほぞん
+        session["user_id"] = user_id[0]
+        return redirect("/list")
+
+@app.route("/logout")
+def logout():
+    session.pop("user_id", None)
+    return redirect("/login")
+
+
 
 @app.errorhandler(404)
 def page_not_found(error):
